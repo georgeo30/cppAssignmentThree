a8bdc8207ebe57585badfda791c2da765a9fc0bb
georgeo <georgeosabu@gmail.com>
2020-03-12 20:42:58 +0200

    header for the huffmanNode.cpp

---
+++ HuffmanNode.h
@@ -0,0 +1,45 @@
+#ifndef _HUFFMANNODE_H
+#define _HUFFMANNODE_H
+#include <string>
+#include <memory>
+
+namespace THNGEO002
+{
+    using namespace std;
+
+class HuffmanNode
+{
+private:
+    char ch;
+    int frequency;
+                               
+public:
+        std::shared_ptr<HuffmanNode> left = nullptr;
+        std::shared_ptr<HuffmanNode> right = nullptr;                                   
+
+    //default constructor
+    HuffmanNode();
+    //constructor
+    HuffmanNode(char c,int freq);
+    //destructor
+    ~HuffmanNode();       
+    //copy constructor
+    HuffmanNode(const HuffmanNode & rhs);
+    //move constructor
+    HuffmanNode(HuffmanNode && rhs);
+
+    //copy assignment op
+    HuffmanNode& operator=(const HuffmanNode & rhs);
+    //move assignment op
+    HuffmanNode& operator=(const HuffmanNode && rhs);
+    //compare perator
+    bool operator<(const HuffmanNode &rhs)const;
+    int getFrequency() const;
+    char getLetter() const;
+
+
+
+};
+
+} // namespace THNGEO002
+#endif
