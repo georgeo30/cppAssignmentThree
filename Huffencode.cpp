eff0d287de70c5d4fb057f59bd628fed2c6bafd9
georgeo <georgeosabu@gmail.com>
2020-03-13 12:05:41 +0200

    defined the construcotrs

--- Huffencode.cpp
+++ Huffencode.cpp
@@ -17,24 +17,42 @@ Huffencode::Huffencode(string inF, string outF)
 Huffencode::~Huffencode()
 {
 }
+//copy constructor
+Huffencode::Huffencode(const Huffencode& rhs):root(new HuffmanNode(*rhs.root)){
+    this->fTable=rhs.fTable;
+
+}
+//move constructor
+Huffencode::Huffencode( Huffencode&& rhs):root(new HuffmanNode(*rhs.root)){
+    this->fTable=move(rhs.fTable);
+    rhs.root.reset();
+    rhs.fTable.clear();
+}
+
 //reading file into unorderedMap
 void Huffencode::readFile()
 {
     char ch;
     int count = 0;
     fstream fin(iFile, fstream::in);
-    while (fin >> noskipws >> ch)
+    while (fin.get(ch))
     {
+        
+        
         if (fTable.find(ch) == fTable.end())
         {
             fTable[ch] = 1;
         }
-        // else
-        // {
-        //     fTable.find(ch)
-        // }
-    }
-    cout << count << endl;
+        else
+        {
+            int inc=fTable.at(ch);
+            inc++;
+            fTable[ch]=inc;
+        }
+        }
+    
+    for (auto x : fTable) 
+      cout<<x.first<<" "<<x.second<<endl; 
 }
 
 } // namespace THNGEO002
